b45d439a002d10d5eb8cf815ae36693b8a0077ab
Prophet-Wei <prophetwei.ee12@nycu.edu.tw>
2024-06-12 03:00:54 +0800

    rm

--- Final/main.py
+++ Final/main.py
@@ -1,11 +1,4 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1600, 1200))
 
-running = True
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
